2c49ac43e16b13dead141b7aee7ded229c996986
kishan kumar <kkishan768@gmail.com>
2026-08-21 20:54:32 +0530

    created folder structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Kishan.dev</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <input type="checkbox" className="toggle theme-controller" />
+      </div>
+    </div>
+  );
+};
+export default Navbar;
